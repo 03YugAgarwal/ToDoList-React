0b4fb7f74287a5ae6d2edc0fb636131e7c3f71ed
Yug <03yugagarwal@gmail.com>
2022-09-16 17:08:22 +0530

    added google oauth2

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,7 @@
+require('dotenv').config()
 const express = require('express')
+const session  = require('express-session')
+const passport = require('passport')
 
 const app = express();
 const PORT = 3333;
@@ -8,7 +11,10 @@ require("./database")
 // Routes
 const authRouter = require('./routes/Auth')
 
+app.use(session({secret: process.env.SESSION_SECRET}))
 app.use('/auth',authRouter);
+app.use(passport.session())
+
 
 
 app.listen(PORT,()=>{
--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,9 +1,39 @@
 const {Router} = require('express')
 
 const router = Router();
+const passport = require('passport')
+require('../stratergies/google')
 
-router.use("/login",(req,res)=>{
-    res.send("Login route")
+router.use(passport.session())
+
+const isLoggedIn = (req,res,next)=>{
+    req.user ? next() : res.sendStatus(401);
+}
+
+router.get("/google",
+    passport.authenticate('google',{scope:['email','profile']})
+)
+
+router.get("/google/callback",
+    passport.authenticate('google',{successRedirect:'/auth/protected',failureRedirect:'/auth/login'})
+)
+
+router.get("/protected",isLoggedIn,(req,res)=>{
+    res.send(`Successful <br> <a href="/auth/logout">Logout</a> Hello ${req.user.displayName}`)
+
 })
+
+router.get("/logout",(req,res)=>{
+    req.logout();
+    req.session.destroy();
+    res.send('Logged out')
+
+})
+
+router.get("/login",(req,res)=>{
+    res.send('<a href="/auth/google">Authenticate using google</a>')
+    // res.send("Successful")
+})
+
 
 module.exports = router;
